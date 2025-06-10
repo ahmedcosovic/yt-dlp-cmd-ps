d78b92616c4c7298f01b525dda3dbbc04a2d4e4d
Ahmed Ćosović <115624319+ahmedcosovic@users.noreply.github.com>
2025-06-10 17:42:57 +0200

    back up binary before update

--- scripts/updateBinary.ps1
+++ scripts/updateBinary.ps1
@@ -1,18 +1,32 @@
-﻿New-Item -ItemType Directory ".\bin" -Force -Verbose
+# yt-dlp
+
+New-Item -ItemType Directory ".\bin" -Force -Verbose
 $binaryLocation = ".\bin\yt-dlp.exe"
-
-$curlPs = Invoke-WebRequest -Uri "https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest"
-$downloadLink32 = ($curlPs.Content | ConvertFrom-Json).assets.browser_download_url | Where-Object { $_ -like "*yt-dlp_x86.exe" }
-$downloadLink64 = ($curlPs.Content | ConvertFrom-Json).assets.browser_download_url | Where-Object { $_ -like "*yt-dlp.exe" }
-
-if ([Environment]::Is64BitOperatingSystem) {
-    $finalDownloadLink = $downloadLink64
-} else {
-    $finalDownloadLink = $downloadLink32
-}
+$backupLocation = ".\temp\yt-dlp_bkp.exe"
 
 if (Test-Path $binaryLocation) {
-    Remove-Item $binaryLocation -Force -Verbose
+	Move-Item -Path $binaryLocation -Destination $backupLocation -ErrorAction Stop
 }
 
-Invoke-WebRequest -Uri $finalDownloadLink -OutFile $binaryLocation
+try {
+	$curlPs = Invoke-WebRequest -Uri "https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest"
+	$downloadLink32 = ($curlPs.Content | ConvertFrom-Json).assets.browser_download_url | Where-Object { $_ -like "*yt-dlp_x86.exe" }
+	$downloadLink64 = ($curlPs.Content | ConvertFrom-Json).assets.browser_download_url | Where-Object { $_ -like "*yt-dlp.exe" }
+
+	if ([Environment]::Is64BitOperatingSystem) {
+		$finalDownloadLink = $downloadLink64
+	} else {
+		$finalDownloadLink = $downloadLink32
+	}
+
+	if (Test-Path $binaryLocation) {
+		Remove-Item $binaryLocation -Force -Verbose
+	}
+
+	Invoke-WebRequest -Uri $finalDownloadLink -OutFile $binaryLocation
+} catch {
+	if (Test-Path $backupLocation) {
+		Move-Item -Path $backupLocation -Destination $binaryLocation -ErrorAction Stop
+	}
+	Write-Host "Error: $_"
+}
